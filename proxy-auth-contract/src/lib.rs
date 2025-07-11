bef03a514862a8491b8d47a7326fb7d0e62ddc8a
Gustavo Fuhr <gustavo@wbi.dev>
2025-07-10 21:28:09 -0300

    cleaning

--- proxy-auth-contract/src/lib.rs
+++ proxy-auth-contract/src/lib.rs
@@ -1,14 +1,8 @@
-// --- contract
 pub mod contract;
 pub mod error;
-
-// pub mod helpers
-pub mod integration_tests;
 pub mod utils;
 pub mod msg;
 pub mod state;
 pub mod execute;
 pub mod query;
-
-// --- errors
 pub use crate::error::ContractError;
